3590cddcc93b6ac189bc65535db20253196bfb40
Samson Alajede <ajesamson@yahoo.com>
2025-07-13 11:08:17 +0100

    Added migrations for wallets database table

---
+++ migrations/20250712160043_create_wallets_table.ts
@@ -0,0 +1,24 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('wallets', (table) => {
+    table.increments('id').primary();
+    table.uuid('uid').defaultTo(knex.fn.uuid());
+    table
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
+    table.decimal('balance', 10, 2).notNullable().defaultTo(0.0);
+    table.string('currency', 3).notNullable().defaultTo('NGN');
+    table.boolean('is_active').defaultTo(true);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('wallets');
+}
