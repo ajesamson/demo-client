279a5b420becaf4c3a29a29a6e202ee4a4c7aadc
Samson Alajede <ajesamson@yahoo.com>
2025-07-14 11:43:38 +0100

    Added wallet entity for table type reference

---
+++ src/wallets/entities/wallet.entity.ts
@@ -0,0 +1,10 @@
+export interface Wallet {
+  id: number;
+  uid: string;
+  user_id: number;
+  balance: string;
+  currency: string;
+  is_active: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
