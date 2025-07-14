e550bb44ad15d28d4471650afb287531f4461f9f
Samson Alajede <ajesamson@yahoo.com>
2025-07-14 23:59:10 +0100

    Created transfer interface for type management

---
+++ src/transfers/entities/transfer.entity.dto.ts
@@ -0,0 +1,12 @@
+export interface Transfer {
+  id: number;
+  uid: string;
+  sender_wallet_id: number;
+  receiver_wallet_id: number;
+  amount: string;
+  currency: string;
+  description: string;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+}
