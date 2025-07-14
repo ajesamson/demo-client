cbe8d99a372444c0a3f9d2dc89f51ffb9c62fb07
Samson Alajede <ajesamson@yahoo.com>
2025-07-14 11:45:55 +0100

    Fixed forward reference errors

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { HttpModule } from '@nestjs/axios';
+import { WalletsModule } from 'src/wallets/wallets.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [HttpModule],
+  imports: [HttpModule, forwardRef(() => WalletsModule)],
+  exports: [UsersService],
 })
 export class UsersModule {}
