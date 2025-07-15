7d720b07d65856a0c455225f7f3a02905e621d02
Samson Alajede <ajesamson@yahoo.com>
2025-07-15 13:55:19 +0100

    Updated with new transaction response dto path

--- src/transactions/dto/transaction-transfer-response.dto.ts
+++ src/transactions/dto/transaction-transfer-response.dto.ts
@@ -1,5 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
-import { TransactionResponseDto } from './trasaction-response.dto';
+import { TransactionResponseDto } from './transaction-response.dto';
 import { TransferResponseDto } from 'src/transfers/dtos/transfer-response.dto';
 import { Expose, Type } from 'class-transformer';
 
