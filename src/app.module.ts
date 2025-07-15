a0d0d0c5a932e3872ac06e37d2b1194c1b8ab1af
Samson Alajede <ajesamson@yahoo.com>
2025-07-15 12:47:46 +0100

    Exposed newly added auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { UsersModule } from './users/users.module';
 import { WalletsModule } from './wallets/wallets.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { TransfersModule } from './transfers/transfers.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [KnexModule, UsersModule, WalletsModule, TransactionsModule, TransfersModule],
+  imports: [
+    KnexModule,
+    UsersModule,
+    WalletsModule,
+    TransactionsModule,
+    TransfersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
