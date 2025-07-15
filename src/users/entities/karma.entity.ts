4e9c0747a22e7ed5fc9ec3e0470010d6d36f3851
Samson Alajede <ajesamson@yahoo.com>
2025-07-15 12:40:22 +0100

    Created an entity for karma blacklist response

---
+++ src/users/entities/karma.entity.ts
@@ -0,0 +1,34 @@
+export interface KarmaEntity {
+  status: string;
+  message: string;
+  data: KarmaData;
+  meta: KarmaMeta;
+}
+
+interface KarmaData {
+  karma_identity: string;
+  amount_in_contention: string;
+  reason: string | null;
+  default_date: string;
+  karma_type: KarmaType;
+  karma_identity_type: KarmaIdentityType;
+  reporting_entity: ReportingEntity;
+}
+
+interface KarmaType {
+  karma: string;
+}
+
+interface KarmaIdentityType {
+  identity_type: string;
+}
+
+interface ReportingEntity {
+  name: string;
+  email: string;
+}
+
+interface KarmaMeta {
+  cost: number;
+  balance: number;
+}
