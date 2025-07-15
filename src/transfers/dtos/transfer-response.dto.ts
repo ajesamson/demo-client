e0a690c755697514e0707c65f9df7a94562f5af7
Samson Alajede <ajesamson@yahoo.com>
2025-07-15 16:04:03 +0100

    Added response id  to transfer response dto

--- src/transfers/dtos/transfer-response.dto.ts
+++ src/transfers/dtos/transfer-response.dto.ts
@@ -1,6 +1,9 @@
 import { IsDecimal, IsInt, IsOptional, IsString } from 'class-validator';
 
 export class TransferResponseDto {
+  @IsString()
+  id: string;
+
   @IsInt()
   sender_wallet_id: number;
 
