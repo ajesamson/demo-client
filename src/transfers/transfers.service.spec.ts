ea53720868aab108e9b472228004c619aadfe2ed
Samson Alajede <ajesamson@yahoo.com>
2025-07-17 22:22:29 +0100

    Added mocks for transfer repository

--- src/transfers/transfers.service.spec.ts
+++ src/transfers/transfers.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransfersService } from './transfers.service';
+import { TransferRepository } from './repositories/transfers.repository';
 
 describe('TransfersService', () => {
   let service: TransfersService;
+  const mockTransferRepository = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransfersService],
+      providers: [
+        TransfersService,
+        {
+          provide: TransferRepository,
+          useValue: mockTransferRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TransfersService>(TransfersService);
