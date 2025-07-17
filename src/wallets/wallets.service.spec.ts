7279ff1cfbb3a70e406d69bb559c5d268f551547
Samson Alajede <ajesamson@yahoo.com>
2025-07-17 14:02:38 +0100

    Added create and update test implementation for wallets service

--- src/wallets/wallets.service.spec.ts
+++ src/wallets/wallets.service.spec.ts
@@ -1,12 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WalletsService } from './wallets.service';
+import { UsersService } from 'src/users/users.service';
+import { WalletRepository } from './repositories/wallet.repository';
+import { Knex } from 'knex';
+import { BadRequestException } from '@nestjs/common';
+import { createWalletDtoStub } from './factories/wallet.factory';
+import { userStud } from 'src/users/factories/user.factory';
 
 describe('WalletsService', () => {
   let service: WalletsService;
+  const mockUsersService = {
+    findByUid: jest.fn(),
+  };
+  const mockRepo = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findByField: jest.fn(),
+    update: jest.fn(),
+  };
+  const trx = {} as Knex.Transaction;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WalletsService],
+      providers: [
+        WalletsService,
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+        {
+          provide: WalletRepository,
+          useValue: mockRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<WalletsService>(WalletsService);
@@ -15,4 +41,55 @@ describe('WalletsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should create wallet for user with id', async () => {
+      mockRepo.create.mockReturnValue(1);
+
+      const result = await service.create(1, trx);
+
+      expect(result).toEqual(1);
+      expect(mockRepo.create).toHaveBeenCalledWith(1, trx);
+    });
+
+    it('should throw BadRequestException for user with existing wallet', async () => {
+      mockRepo.create.mockReturnValue(undefined);
+
+      await expect(service.create(1, trx)).rejects.toThrow(BadRequestException);
+      expect(mockRepo.create).toHaveBeenCalledWith(1, trx);
+    });
+  });
+
+  describe('createByUsersUid', () => {
+    it('should create wallet for user with uid', async () => {
+      mockUsersService.findByUid.mockResolvedValue(userStud);
+      jest.spyOn(service, 'create').mockResolvedValue(1);
+
+      const result = await service.createByUsersUid(createWalletDtoStub);
+
+      expect(result).toEqual(1);
+      expect(mockUsersService.findByUid).toHaveBeenCalledWith(
+        createWalletDtoStub.user_id,
+      );
+      expect(service.create).toHaveBeenCalledWith(1);
+    });
+  });
+
+  describe('updateBalance', () => {});
+
+  describe('findByUserId', () => {});
+
+  describe('findUserWallet', () => {});
+
+  describe('findById', () => {});
+
+  describe('findByUid', () => {});
+
+  describe('findByUidList', () => {});
+
+  describe('findAll', () => {});
+
+  describe('findOne', () => {});
+
+  describe('findWalletTransactions', () => {});
 });
