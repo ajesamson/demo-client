ba735cd80b5c697aa4817e41e157fedc4fd8e03b
Samson Alajede <ajesamson@yahoo.com>
2025-07-17 22:16:59 +0100

    Updated app controller spec to test for health status

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,19 +4,23 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  const mockAppUser = {
+    getHealth: jest.fn(),
+  };
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [{ provide: AppService, useValue: mockAppUser }],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return active status', () => {
+      mockAppUser.getHealth.mockReturnValue({ status: 'Active' });
+      expect(appController.getHealth()).toEqual({ status: 'Active' });
     });
   });
 });
