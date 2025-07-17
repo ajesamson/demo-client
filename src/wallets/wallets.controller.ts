609ba9fba1ffe5cc109aeed05888e27856e391d4
Samson Alajede <ajesamson@yahoo.com>
2025-07-17 13:59:03 +0100

    Removed unused path endpoint and refactored create with only one call to service

--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -8,25 +8,18 @@ import {
   Post,
 } from '@nestjs/common';
 import { WalletsService } from './wallets.service';
-import { UpdateWalletDto } from './dto/update-wallet.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CreateWalletDto } from './dto/create-wallet.dto';
-import { UsersService } from 'src/users/users.service';
 
 @ApiTags('Wallets')
 @ApiBearerAuth()
 @Controller('wallets')
 export class WalletsController {
-  constructor(
-    private readonly walletsService: WalletsService,
-    private readonly userService: UsersService,
-  ) {}
+  constructor(private readonly walletsService: WalletsService) {}
 
   @Post()
   async create(@Body() dto: CreateWalletDto) {
-    const user = await this.userService.findByUid(dto.user_id);
-    const _walletId = await this.walletsService.create(user.id);
-    return await this.walletsService.findByUserId(user.id);
+    return await this.walletsService.createByUsersUid(dto);
   }
 
   @Get()
@@ -42,13 +35,5 @@ export class WalletsController {
   @Get(':uid/transactions')
   async findTransactions(@Param('uid', new ParseUUIDPipe()) uid: string) {
     return await this.walletsService.findWalletTransactions(uid);
-  }
-
-  @Patch(':uid')
-  async update(
-    @Param('uid', new ParseUUIDPipe()) uid: string,
-    @Body() updateWalletDto: UpdateWalletDto,
-  ) {
-    return await this.walletsService.update(uid, updateWalletDto);
   }
 }
