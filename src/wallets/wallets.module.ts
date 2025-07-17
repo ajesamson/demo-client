da1711434700e9dadc0873c2a4f39f4a38d86d4e
Samson Alajede <ajesamson@yahoo.com>
2025-07-17 09:46:19 +0100

    Added wallet repository to the module

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -2,10 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
 import { UsersModule } from 'src/users/users.module';
+import { WalletRepository } from './repositories/wallet.repository';
 
 @Module({
   controllers: [WalletsController],
-  providers: [WalletsService],
+  providers: [WalletsService, WalletRepository],
   exports: [WalletsService],
   imports: [forwardRef(() => UsersModule)],
 })
