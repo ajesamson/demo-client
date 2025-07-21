085bcf7f6705a382b8358e77e274cb8c37fe1a5d
Samson Alajede <ajesamson@yahoo.com>
2025-07-21 10:26:42 +0100

    Added logging to the appservice gethealth

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger, LoggerService } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject(Logger) private readonly logger: LoggerService) {}
+
   getHealth(): { status: string } {
+    this.logger.log('Getting application health', AppService.name);
     return { status: 'Active' };
   }
 }
