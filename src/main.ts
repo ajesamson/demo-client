6e1e1b9076b4623aba32fe667f697038fc6e2e2e
Samson Alajede <ajesamson@yahoo.com>
2025-07-21 11:04:31 +0100

    Added versioning and fixed missing api name in route outside swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from './logger/winston-config';
 
@@ -16,12 +16,17 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Demo Credit')
     .setDescription('Mobile Lending App API')
-    .setVersion('0.1')
+    .setVersion('1.0')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
